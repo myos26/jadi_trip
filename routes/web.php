f83b5ab7ca05c248f18cb68954d5f24e468e69df
Bayik4 <ayikarbail@gmail.com>
2024-06-15 13:50:24 +0700

    fi halaman admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use App\Models\User;
 
 /*
@@ -57,3 +59,13 @@ Route::get('/article', function () {
 });
 
 Route::get('/logout', [AuthController::class, 'logout']);
+
+// ADMIN ROUTES
+Route::get('/Dashboard', [AdminController::class, 'Dashboard']);
+
+Route::get('/profil', [ProfileController::class, 'index']);
+
+Route::get('/postingan', [PostController::class, 'index']);
+Route::get('/tambahpostingan', [PostController::class, 'TambahPostingan']);
+
+Route::post('/upload', [PostController::class, 'upload'])->name('ckeditor.upload');

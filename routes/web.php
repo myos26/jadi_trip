0a2df27ee22b3efadabb0d10d1651227db95b716
Masyos <152259574+myos26@users.noreply.github.com>
2024-07-10 10:22:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -56,17 +56,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/info', [AuthController::class, 'info']);
     Route::post('/resend-code', [AuthController::class, 'resendCode']);
     Route::get('/logout', [AuthController::class, 'logout']);
-
-    // Route::get('/dashboard', [AdminController::class, 'Dashboard']);
     Route::get('/user/profil', [ProfileController::class, 'index']);
-    // Route::get('/postingan', [PostController::class, 'index']);
-
-    // iklan routes
-    // Route::get('/iklan', [IklanController::class, 'iklan']);
-
-
-
-    // profile routes
     Route::put('/update/profile/{id}', [ProfileController::class, 'update']);
 });
 
@@ -117,33 +107,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::delete('/akun-delete/{id}', [AkunController::class, 'destroy']);
 });
 
-// Route::middleware(['auth','admin'])->group(function () {
-//     // ADMIN ROUTES
-
-//     Route::get('/dashboard', [AdminController::class, 'Dashboard']);
-//     Route::get('/Dashboard', [AdminController::class, 'Dashboard']);
-//     // Route::get('/profil', [ProfileController::class, 'index']);
-
-//     // post routes
-//     Route::get('/postingan', [PostController::class, 'index']);
-//     Route::get('/create', [PostController::class, 'create']);
-//     Route::post('/upload', [PostController::class, 'upload']);
-//     Route::post('/save-post', [PostController::class, 'store']);
-//     Route::get('/update/{id}', [PostController::class, 'updateView']);
-//     Route::post('/update-post/{id}', [PostController::class, 'update']);
-//     Route::get('/delete/{id}', [PostController::class, 'destroy']);
-
-//     // kategori routes
-//     Route::post('/kategori', [PostController::class, 'storeKategori']);
-//     Route::get('/kategori/delete/{id}', [PostController::class, 'deleteKategori']);
-
-//     // iklan routes
-//     Route::get('/iklan', [IklanController::class, 'index']);
-//     Route::post('/inputIklan', [IklanController::class, 'store']);
-//     Route::post('/iklan/updateStatus/{id}', [IklanController::class, 'updateStatus'])->name('iklan.updateStatus');
-//     Route::post('/iklan/expireAd/{id}', [IklanController::class, 'expireAd'])->name('iklan.expireAd');
-// });
-
 // route blog
 Route::get('/paket/{tipe}/{slug}', [HomeController::class, 'detailPaket']);
 Route::get('/paket/{kategori}', [HomeController::class, 'pakets']);
@@ -184,10 +147,10 @@ Route::get('/sitemap.xml', function () {
     }
 
 
-    // $packages = Paket::all();
-    // foreach ($packages as $package) {
-    //     $sitemap->add(Url::create("/paket/{$package->slug}")->setPriority(0.7));
-    // }
+     $packages = Paket::all();
+     foreach ($packages as $package) {
+         $sitemap->add(Url::create("/paket/{$package->tipe}/{$package->slug}")->setPriority(0.7));
+    }
 
     return $sitemap->toResponse(request());
 });
